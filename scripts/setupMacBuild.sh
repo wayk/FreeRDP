c23d993217bd8729f09cb92aa4178b02b0afb812
Xfortin <Devolutions33@Devolutions33.local>
2016-07-18 14:29:31 -0400

    Update the setupMacBuild script.

--- scripts/setupMacBuild.sh
+++ scripts/setupMacBuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export OPENSSL_ROOT_DIR=external/openssl/
-cmake -DCMAKE_OSX_ARCHITECTURES="i386" -DWITH_SSE2=on -DWITH_CUPS=on -G Xcode .
+#export OPENSSL_ROOT_DIR=external/openssl/
+cmake -DCMAKE_OSX_ARCHITECTURES="i386" -DOPENSSL_INCLUDE_DIR=external/openssl/include/openssl -DOPENSSL_CRYPTO_LIBRARY=external/openssl/lib/libcrypto.a -DOPENSSL_SSL_LIBRARY=external/openssl/lib/libssl.a -DWITH_SSE2=on -DWITH_CUPS=on -G Xcode .
